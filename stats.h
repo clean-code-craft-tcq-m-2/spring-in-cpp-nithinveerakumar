09d49ddd8f94683cb7e84f07a46e3ce167c7218d
Nithin veerakumar <nithinveerakumar@users.noreply.github.com>
2022-10-18 21:51:14 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,31 @@
 #include <vector>
 
+class Stats {
+public:
+    float average = NAN;
+    float min = NAN;
+    float max = NAN;
+};
+
+class EmailAlert {
+public:
+    bool emailSent = false;
+};
+class LEDAlert {
+public:
+    bool ledGlows = false;
+};
+class IAlerter :public EmailAlert, public LEDAlert {
+
+};
+class StatsAlerter {
+    float maxThreshold;
+    IAlerter alerters;
+    StatsAlerter(float mt, IAlerter a) :maxThreshold(mt), alerters(a) {}
+    void checkAndAlert(const std::vector<float>&);
+};
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+   
+  
+    Stats ComputeStatistics(const std::vector<float>& );
 }
